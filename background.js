877b6bcb02738af743588046207c5a1b64ef7496
chandrakishorSingh <chandrakishors52@gmail.com>
2026-04-24 21:50:58 +0530

    Implement basic redirection

---
+++ background.js
@@ -0,0 +1,43 @@
+let count = 0;
+
+const createClassicUrl = (url) => {
+    return url.replace("https://leetcode.com/", "https://leetcode.com/classic/");
+};
+
+
+const isUrlMatchingPattern = (url) => {
+    if (url === null || url === undefined) {
+        return false;
+    }
+
+    const regex = /^https:\/\/leetcode\.com\/problems\/([a-zA-Z0-9\-]+)\/?(description\/?)?$/;
+    const matches = url.match(regex);
+
+    if (matches === null || matches === undefined || matches.length === 0) {
+        console.log(`not match url: ${url}`)
+        return false;
+    }
+    
+    console.log("match length: " + matches.length);
+    console.log("match 1: " + matches[0]);
+    console.log("match 2: " + matches[1]);
+    console.log("match 3: " + matches[2]);
+    return true;
+};
+
+chrome.webNavigation.onBeforeNavigate.addListener(
+    (details) => {
+        count++;
+        console.log("call count: " + count);
+        console.log("Url: " + details.url);
+        console.log("tabId: " + details.tabId);
+
+        const url = details.url;
+        const tabId = details.tabId;
+
+        if (isUrlMatchingPattern(url)) {
+            const newUrl = createClassicUrl(url);
+            chrome.tabs.update(tabId, { url: newUrl });
+        }
+    }
+);
---
+++ popup.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("toggle");
+
+toggleButton.addEventListener("click", () => {
+    console.log("click");
+    console.log(window.location.href);
+})
