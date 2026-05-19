9212e51d478ce747e45065d534c8b7af869c2eba
chandrakishorSingh <chandrakishors52@gmail.com>
2026-05-19 08:21:48 +0530

    Feat: Configure extension to be enable by default

--- src/background.js
+++ src/background.js
@@ -1,12 +1,10 @@
 const browserApi = typeof browser !== 'undefined' ? browser : chrome;
 
-let count = 0;
 const EXTENSION_ENABLE_STATE_KEY = "EXTENSION_ENABLE_STATE_KEY";
 
 const createClassicUrl = (url) => {
     return url.replace("https://leetcode.com/", "https://leetcode.com/classic/");
 };
-
 
 const isUrlMatchingPattern = (url) => {
     if (url === null || url === undefined) {
@@ -16,7 +14,7 @@ const isUrlMatchingPattern = (url) => {
     // regex for matching leetcode problem urls
     // modify it so that it also match when url has query parameters
     // example: https://leetcode.com/problems/two-sum/?envType=study-plan&id=algorithm-i
-    
+
     const regex = /^https:\/\/leetcode\.com\/problems\/([a-zA-Z0-9\-]+)\/?(description\/?)?(\?.*)?$/;
     const matches = url.match(regex);
 
@@ -24,7 +22,7 @@ const isUrlMatchingPattern = (url) => {
         console.log(`not match url: ${url}`)
         return false;
     }
-    
+
     console.log("match length: " + matches.length);
     console.log("match 1: " + matches[0]);
     console.log("match 2: " + matches[1]);
@@ -32,48 +30,65 @@ const isUrlMatchingPattern = (url) => {
     return true;
 };
 
-browserApi.webNavigation.onBeforeNavigate.addListener(
-    (details) => {
+// check if a key exists in the storage
+const isKeyExists = async (key) => {
+    const details = await browserApi.storage.local.get(key);
+    if (details[key] !== undefined) {
+        console.log(`${key} is: ${details[key]}`);
+        return details[key];
+    }
+
+    console.log(`${key} is: undefined`);
+    return undefined;
+};
+
+// store a given key-value pair
+const setKeyValuePair = async (key, value) => {
+    await browserApi.storage.local.set({ [key]: value });
+};
+
+// on installed listener
+browserApi.runtime.onInstalled.addListener(async () => {
+    console.log("On installed: ", new Date().toLocaleDateString());
+
+    const value = await isKeyExists(EXTENSION_ENABLE_STATE_KEY);
+    if (value === undefined) {
+        console.log('No flag exists on installed');
+        await setKeyValuePair(EXTENSION_ENABLE_STATE_KEY, true);
+    } else {
+        console.log('Flag exists already');
+    }
+});
+
+// onBeforeNavigate listener
+browserApi.webNavigation.onBeforeNavigate.addListener(async (details) => {
         if (!details || details.frameId !== 0 || typeof details.tabId !== 'number' || details.tabId < 0) {
             return;
         }
 
         const frameId = details.frameId;
-        // const parentFrameId = details.parentFrameId;
-        // const processId = details.processId;
         const tabId = details.tabId;
-        // const timeStamp = details.timeStamp;
-        // const url = details.url;
+        const url = details.url;
         console.log("frameId: " + frameId);
         console.log("tabId: " + tabId);
-
+        console.log("url: " + url);
 
         console.log("onBeforeNavigate called");
 
-        let isEnabled = true;
+        let isEnabled = await isKeyExists(EXTENSION_ENABLE_STATE_KEY);
+        if (isEnabled === undefined || isEnabled == null) {
+            isEnabled = true;
+            await setKeyValuePair(EXTENSION_ENABLE_STATE_KEY, isEnabled);
+        }
 
-        browserApi.storage.local.get([EXTENSION_ENABLE_STATE_KEY], (result) => {
-            if (result.EXTENSION_ENABLE_STATE_KEY === null || result.EXTENSION_ENABLE_STATE_KEY === undefined || result.EXTENSION_ENABLE_STATE_KEY === false) {
-                isEnabled = false;
-            }
+        if (isEnabled === false) {
+            return;
+        }
 
-            if (isEnabled === false) {
-                return;
-            }
-
-            count++;
-            console.log("call count: " + count);
-            console.log("Url: " + details.url);
-            console.log("tabId: " + details.tabId);
-
-            const url = details.url;
-            const tabId = details.tabId;
-
-            if (isUrlMatchingPattern(url)) {
-                const newUrl = createClassicUrl(url);
-                browserApi.tabs.update(tabId, { url: newUrl });
-            }
-        });
+        if (isUrlMatchingPattern(url)) {
+            const newUrl = createClassicUrl(url);
+            browserApi.tabs.update(tabId, { url: newUrl });
+        }
     },
     {
         url: [
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,45 +1,50 @@
 const browserApi = typeof browser !== 'undefined' ? browser : chrome;
 
 const EXTENSION_ENABLE_STATE_KEY = "EXTENSION_ENABLE_STATE_KEY";
-let isEnabled;
 
-console.log("popup.js loaded: " + new Date().toLocaleDateString());
-const toggleButton = document.getElementById("toggle");
-
-const setInitialToggleState = () => {
-    browserApi.storage.local.get([EXTENSION_ENABLE_STATE_KEY], (result) => {
-        if (result.EXTENSION_ENABLE_STATE_KEY === null || result.EXTENSION_ENABLE_STATE_KEY === undefined) {
-            isEnabled = false;
-        } else {
-            isEnabled = result.EXTENSION_ENABLE_STATE_KEY;
-        }
-
-        console.log("Initial toggle state after extension loading: " + isEnabled);
-
-        toggleButton.innerText = isEnabled ? "Disable Old UI" : "Enable Old UI";
-    });
+// store a given key-value pair
+const setKeyValuePair = async (key, value) => {
+    await browserApi.storage.local.set({ [key]: value });
 };
 
-// toggle button click listener
-toggleButton.addEventListener("click", () => {
-    console.log("toggle button clicked");
+// check if a key exists in the storage
+const isKeyExists = async (key) => {
+    const details = await browserApi.storage.local.get(key);
+    if (details[key] !== undefined) {
+        console.log(`${key} is: ${details[key]}`);
+        return details[key];
+    }
 
-    browserApi.storage.local.get([EXTENSION_ENABLE_STATE_KEY], (result) => {
-        if (result.EXTENSION_ENABLE_STATE_KEY === null || result.EXTENSION_ENABLE_STATE_KEY === undefined) {
-            isEnabled = false;
-        } else {
-            isEnabled = result.EXTENSION_ENABLE_STATE_KEY;
-        }
-        
-        console.log(`LeetCode Classic Enabled: ${isEnabled}`);
+    console.log(`${key} is: undefined`);
+    return undefined;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log("popup.js loaded: " + new Date().toLocaleDateString());
+    
+    // get the reference of toggle button
+    const toggleButton = document.getElementById("toggle");
+
+    // toggle button click listener
+    toggleButton.addEventListener("click", async () => {
+        console.log("toggle button clicked");
+
+        let isEnabled = await isKeyExists(EXTENSION_ENABLE_STATE_KEY);
+        isEnabled = isEnabled === undefined ? true : isEnabled;
+
+        console.log(`Initial state Enabled: ${isEnabled}`);
+
 
         isEnabled = !isEnabled;
         toggleButton.innerText = isEnabled ? "Disable Old UI" : "Enable Old UI";
 
-        browserApi.storage.local.set({ EXTENSION_ENABLE_STATE_KEY: isEnabled });
+        await setKeyValuePair(EXTENSION_ENABLE_STATE_KEY, isEnabled);
         console.log(`After toggle click, LeetCode Classic Enabled: ${isEnabled}`);
     });
 
-});
 
-setInitialToggleState();
+    let isEnabled = await isKeyExists(EXTENSION_ENABLE_STATE_KEY);
+    isEnabled = isEnabled === undefined ? true : isEnabled;
+
+    toggleButton.innerText = isEnabled ? "Disable Old UI" : "Enable Old UI";
+});
